3bfafa91cf1d1be1cbf153275975623fc9ebe96c
eleonorastucchi <71377204+eleonorastucchi@users.noreply.github.com>
2021-01-18 00:26:50 +0100

    Project updated 17.1

--- sketch1.js
+++ sketch1.js
@@ -7,12 +7,14 @@ function preload(){
 gray_bar = loadImage("./assets/images/gray_bar.png");
 icon = loadImage("./assets/images/icon.png");
 sound = loadSound("./assets/sounds/enter_sound.mp3");
-
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(0, 128, 130);
+
+  sound.setVolume(0.10);
+  sound.play();
 
 
 //buttonIcon
@@ -28,6 +30,7 @@ imageMode(CENTER);
 image(gray_bar, windowWidth / 2, windowHeight * 25/26, gray_bar.width / 2.79, gray_bar.height / 2.79);
 image(icon, windowWidth / 2, windowHeight / 2, icon.width / 2, icon.height / 2);
 }
+
 
 function newSketch1() {
   window.open("index2.html", "_self");
--- sketch2.js
+++ sketch2.js
@@ -1,22 +1,65 @@
 let paint;
+let myCanvas;
+let myButton;
+let hSlide, sSlide, bSlide;
+let h, s, b;
 
 function preload(){
 paint = loadImage("./assets/images/paint.png");
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background("white");
+  createCanvas(windowWidth, windowHeight);
+  myCanvas = createGraphics(windowWidth, windowHeight);
 
-//buttonIcon
-// push();
-// buttonIcon = createButton("Create your background image");
-// buttonIcon.position(windowWidth / 2 - buttonIcon.width / 2, windowHeight / 1.7 - buttonIcon.height / 2);
-// buttonIcon.mousePressed();
-// pop();
+  colorMode(HSB);
+      // create the sliders
+      hSlide = createSlider(0, 360, 180); // low val, high val, default
+      sSlide = createSlider(0, 100, 50);
+      bSlide = createSlider(0, 100, 50);
+
+      // position the sliders
+      hSlide.position(windowWidth / 150, windowHeight * 2.5 / 20);
+      sSlide.position(windowWidth / 150, windowHeight * 4 / 20);
+      bSlide.position(windowWidth / 150, windowHeight * 5.5 / 20);
+
+      // variables to hold the color
+      h = 180;
+      s = 50;
+      b = 50;
+
+  push();
+  myButton = createButton("Save");
+  myButton.position(windowWidth/21-myButton.width/2, windowHeight*8/9-myButton.height/2);
+  myButton.mousePressed(myCanvas.save);
+  pop();
 }
 
 function draw() {
-imageMode(CENTER);
-image(paint, windowWidth / 2, windowHeight / 2, paint.width / 1.5, paint.height / 1.75);
+  imageMode(CENTER);
+  image(paint, windowWidth / 2, windowHeight / 2, paint.width / 1.5, paint.height / 1.75);
+myCanvas.background(h, s, b);
+
+    // position text
+    push();
+    textAlign(LEFT, TOP);
+    textFont("Arimo");
+    textSize(15);
+    stroke("black")
+    strokeWeight(0.7);
+    fill("black");
+    text('Hue: ' + hSlide.value(), windowWidth / 150, windowHeight * 3 / 20);
+    text('Saturation: '+ sSlide.value(), windowWidth / 150, windowHeight * 4.5 / 20);
+    text('Brightness: '+ bSlide.value(), windowWidth / 150, windowHeight * 6 / 20);
+    pop();
+
+    // read the value of the slider and store it in a variable
+    h = hSlide.value();
+    s = sSlide.value();
+    b = bSlide.value();
+
+}
+
+function myButtonPress(){
+ myCanvas.save("NewScreensaver.jpg");
 }
