4a9c6d8de94789bf32da3cba998a6dafc21927ff
eleonorastucchi <71377204+eleonorastucchi@users.noreply.github.com>
2021-01-19 00:38:55 +0100

    Updated project 18.1

--- sketch2.js
+++ sketch2.js
@@ -1,65 +1,106 @@
 let paint;
-let myCanvas;
 let myButton;
 let hSlide, sSlide, bSlide;
 let h, s, b;
+let pg;
+let slide;
 
 function preload(){
 paint = loadImage("./assets/images/paint.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  myCanvas = createGraphics(windowWidth, windowHeight);
+createCanvas(windowWidth, windowHeight);
+pg = createGraphics(windowWidth, windowHeight);
+
+background(h, s, b);
 
   colorMode(HSB);
-      // create the sliders
-      hSlide = createSlider(0, 360, 180); // low val, high val, default
-      sSlide = createSlider(0, 100, 50);
-      bSlide = createSlider(0, 100, 50);
+      //create the sliders
+      hSlide = createSlider(0, 360, 360);
+      sSlide = createSlider(0, 100, 80);
+      bSlide = createSlider(0, 100, 80);
+      slide = createSlider(0, 70, 20);
 
-      // position the sliders
-      hSlide.position(windowWidth / 150, windowHeight * 2.5 / 20);
-      sSlide.position(windowWidth / 150, windowHeight * 4 / 20);
-      bSlide.position(windowWidth / 150, windowHeight * 5.5 / 20);
+      //position the sliders
+      hSlide.position(windowWidth / 150, windowHeight * 3 / 20);
+      sSlide.position(windowWidth / 150, windowHeight * 6 / 20);
+      bSlide.position(windowWidth / 150, windowHeight * 9 / 20);
+      slide.position(windowWidth / 150, windowHeight * 12 / 20);
 
       // variables to hold the color
       h = 180;
       s = 50;
       b = 50;
 
+  p1 = createP("Move the sliders to customize the canvas");
+  p1.position(windowWidth / 2.5, windowHeight * 9.1 / 10);
+
+  p2 = createP("Click Save to download your new desktop background");
+  p2.position(windowWidth / 2.7, windowHeight * 9.3 / 10);
+
+  p3 = createP("Hue");
+  p3.position(windowWidth / 150, windowHeight * 3.3 / 20);
+
+  p4 = createP("Saturation");
+  p4.position(windowWidth / 150, windowHeight * 6.3 / 20);
+
+  p5 = createP("Brightness");
+  p5.position(windowWidth / 150, windowHeight * 9.3 / 20);
+
+  p6 = createP("Circles");
+  p6.position(windowWidth / 150, windowHeight * 12.3 / 20);
+
   push();
   myButton = createButton("Save");
   myButton.position(windowWidth/21-myButton.width/2, windowHeight*8/9-myButton.height/2);
-  myButton.mousePressed(myCanvas.save);
+  myButton.mousePressed(printBackground);
   pop();
-}
+
+  }
 
 function draw() {
+
+push();
+  h = hSlide.value();
+  s = sSlide.value();
+  b = bSlide.value();
+
+  pg.background(h, s, b);
+pop();
+
+for (let scl = 0; scl < slide.value(); scl++) {
+
+image(pg, windowWidth / 2, windowHeight / 2);
+
+push();
+    pg.noFill();
+    pg.strokeWeight(1);
+    pg.stroke("white");
+    pg.ellipse(windowWidth / 2, windowHeight / 2.1, scl * 10, scl * 10);
+pop();
+
+push();
+    pg.noFill();
+    pg.strokeWeight(1);
+    pg.stroke("black");
+    pg.ellipse(windowWidth / 3, windowHeight / 2.1, scl * 10, scl * 10);
+pop();
+
+push();
+    pg.noFill();
+    pg.strokeWeight(1);
+    pg.stroke("black");
+    pg.ellipse(windowWidth / 1.5, windowHeight / 2.1, scl * 10, scl * 10);
+pop();
+
+  }
+
   imageMode(CENTER);
   image(paint, windowWidth / 2, windowHeight / 2, paint.width / 1.5, paint.height / 1.75);
-myCanvas.background(h, s, b);
-
-    // position text
-    push();
-    textAlign(LEFT, TOP);
-    textFont("Arimo");
-    textSize(15);
-    stroke("black")
-    strokeWeight(0.7);
-    fill("black");
-    text('Hue: ' + hSlide.value(), windowWidth / 150, windowHeight * 3 / 20);
-    text('Saturation: '+ sSlide.value(), windowWidth / 150, windowHeight * 4.5 / 20);
-    text('Brightness: '+ bSlide.value(), windowWidth / 150, windowHeight * 6 / 20);
-    pop();
-
-    // read the value of the slider and store it in a variable
-    h = hSlide.value();
-    s = sSlide.value();
-    b = bSlide.value();
 
 }
 
-function myButtonPress(){
- myCanvas.save("NewScreensaver.jpg");
+function printBackground() {
+save(pg, "test.png");
 }
